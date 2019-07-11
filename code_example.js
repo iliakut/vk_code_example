36453ce54f1d26508a5c92f64c9ed1cc5c5a8ebc
iliakut <32820561+iliakut@users.noreply.github.com>
2019-07-11 10:03:38 +0300

    Create code_example.js

---
+++ code_example.js
@@ -0,0 +1,36 @@
+const request_promise = require('request-promise');
+const fs = require("fs");
+
+// generator of options for request
+function generateOption(id) {
+  return {
+    method: 'GET',
+      uri: 'https://api.vk.com/method/friends.get?v=5.52&',
+      qs: {
+      access_token: 'YOUR_TOKEN',
+        user_id: id,
+        order: 'hints'
+    },
+    json: true
+  };
+}
+
+request_promise(generateOption(17784637))
+  .then((response) => {
+    // response ids arr of 17784637 user
+    let ids_arr = response.response.items;
+    let requestsArr = [];
+    // fill requestArr with request_promise objects with different options
+    for (let i of ids_arr) {
+      requestsArr.push(request_promise(generateOption(i)))
+    }
+    // call Promise.all
+    // !HERE THE PLACE WHERE I CAN CALL "friends.get" METHOD MORE THAN 3 TIMES!
+    return Promise.all(requestsArr);
+  })
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error)
+  });
